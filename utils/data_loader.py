c969785260e5f946f716ceca28dec535a40c2ecf
Kenny Leung <kenleung@stanford.edu>
2018-04-30 17:57:07 -0700

    Moved the GestureFramesDataset class into data_loader.py. Introduced a small function GenerateGestureFramesDataLoader which returns a DataLoader configured for our experiment.

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -9,38 +9,68 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt  # for visualizations
 
+class GestureFramesDataset(Dataset):
+    """Dataset of tensors corresponding to gesture video.
 
-class GestureVideoDataLoader(DataLoader):
-    """Implements the gesture video data loader class.
+    Args:
+        data_dir: str, path to the directory with folders of videos and frames
+        transform: list of callable Classes, e.g. from torchvision.transforms,
+            with which the video frames should be preprocessed
+    """
+    def __init__(self, data_dir, transform):
+        # TODO(kenny): Read in and transform the video frames from the
+        # given information into 4D frame tensors, keeping track of the
+        # classification label along the way.
+        self.data = None
+        self.len = None
 
-    Useful for fetching tensors corresponding to batches of RGBD frames.
-    Uses a configuration file to dynamically construct the dataset, and
-    uses caches/pkls whenever possible, to boost efficiency.
+    def __getitem__(self, gesture_label):
+        # TODO(kenny): Figure out how to sample randomly while keeping the
+        # proportion of labels uniform, despite having a varying number of
+        # videos per label.
+        return self.data[gesture_label]
+
+    def __len__(self):
+        return self.len
+
+
+    def gestures(*args, textfile= "", type_data="kinect"):
+        """Returns dictionary with labels and the locations for the frames.
+
+        Example usage: gestures(5,19,38,37,8,29,213,241,18,92,
+                                textfile = 'train_list_2.txt',
+                                type_data = "kinect")
+        TODO(kenny): Incorporate the following method into GestureFramesDataset.
+        """
+        data = pd.read_csv(textfile, sep=" ", header=None)
+        data.columns = ["rgb", "kinect", "label"]
+        gesture_labels ={}
+        
+        for number in args:
+            labels =[]
+            print(number)
+            directory_labels = data.loc[data['label'] == number][type_data].tolist()
+            for element in directory_labels:
+                labels.append(element[:-4])
+            gesture_labels[number] = labels
+        return gesture_labels
+
+
+def GenerateGestureFramesDataLoader(config):
+    """Returns a configured DataLoader instance.
+
+    Args:
+        config: dict, defines the hyperparameters used when processing and
+            loading dataset information
     """
 
-    # TODO(kenny): Figure out whether we want to incorporate Hiroshi's library
-    # in this class to construct the dataset.
-    def __init__(self, config):
-        self._config = config
-        # TODO(kenny): Initialize the superclass DataLoader using the config
-        # variables, e.g. for batch size and shuffling.
-        dataset = None
-        super(GestureVideoDataLoader, self).__init__(dataset)
-
-
-# TODO(kenny): Incorporate the following method into GestureVideoDataLoader.
-#returns dictionary with labels and the locations for the frames 
-def gestures(*args, textfile= "", type_data="kinect"):
-    data = pd.read_csv(textfile, sep=" ", header=None)
-    data.columns = ["rgb", "kinect", "label"]
-    gesture_labels ={}
-    
-    for number in args:
-        labels =[]
-        print(number)
-        directory_labels = data.loc[data['label'] == number][type_data].tolist()
-        for element in directory_labels:
-            labels.append(element[:-4])
-        gesture_labels[number] = labels
-    return gesture_labels
-gestures = gestures(5,19,38,37,8,29,213,241,18,92, textfile = 'train_list_2.txt', type_data = "kinect")
+    # Build a gesture frames dataset using the configuration information.
+    # This is just dummy code to be replaced.
+    transformed_dataset = GestureFramesDataset(data_dir=None,
+                                               transform=transforms.Compose([
+                                                   Rescale(256),
+                                                   RandomCrop(224),
+                                                   ToTensor()
+                                               ]))
+    return DataLoader(transformed_dataset, batch_size=4,
+                      shuffle=True, num_workers=4)
