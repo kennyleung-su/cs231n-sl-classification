819dd464a4ed4601468f4f9c0a7ace84b63b0080
Kenny Leung <kenleung@stanford.edu>
2018-05-10 22:00:13 -0700

    Some extra comments

--- models/basic.py
+++ models/basic.py
@@ -12,21 +12,20 @@ class PretrainedConvLSTMClassifier(BaseModel):
 	def __init__(self, *args, **kwargs):
 		super(PretrainedConvLSTMClassifier, self).__init__(*args, **kwargs)
 		self._resnet = self._model_config.pretrained_cnn_model(pretrained=True)
-		# for param in self._resnet.parameters():
-		# 	param.requires_grad = False
+		for param in self._resnet.parameters():
+			param.requires_grad = False
 
 		# LSTM unrolls a len <= max_len_seq sequence of 1000d frame vectors.
 		self._lstm = None
 
 	def forward(self, input):
-		"""
-		In the forward function we accept a Tensor of input data and we must return
-		a Tensor of output data. We can use Modules defined in the constructor as
-		well as arbitrary (differentiable) operations on Tensors.
-		"""
+		"""Feeds the input through a pretrained ResNet, then through a variable-length LSTM network
+		followed by a softmax classification layer."""
 		N, C, T, H, W = input.shape
 		example = torch.squeeze(input[:, :, 0, :, :])
 		# Sanity purposes.
 		print(self._resnet(example).shape)
-		cnn_out = np.stack([self._resnet(torch.squeeze(input[:, :, t, :, :])) for t in range(T)], axis=1)
-		return cnn_out
+		cnn_out = torch.stack([self._resnet(torch.squeeze(input[:, :, t, :, :])) for t in range(T)], dim=2)
+
+		# TODO(kenny): Run an LSTM on cnn_out(dim=3) across each of N elements. Will need seq_len.
+		pass
