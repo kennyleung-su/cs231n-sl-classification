88bd9726c73bd76c52713401f296b65665aae58a
Kenny Leung <kenleung@stanford.edu>
2018-05-31 09:25:30 -0700

    Implements lazy loading for the ResNet encoding dataset. Must also be done for the GestureFramesDataset.

--- utils/resnet_encoding_dataset.py
+++ utils/resnet_encoding_dataset.py
@@ -10,7 +10,6 @@ import torch
 from torch.utils.data import Dataset
 
 
-
 class ResnetEncodingDataset(Dataset):
 	"""Dataset of tensors corresponding to gesture video.
 
@@ -21,26 +20,34 @@ class ResnetEncodingDataset(Dataset):
 		self._labels_to_indices_dict = self.map_labels_to_indices(gesture_labels)
 		self._max_example_per_label = max_example_per_label
 		logging.info('Reindexed labels: {0}'.format(self._labels_to_indices_dict))
-		self.data, self.max_seq_len = self.populate_encoding_data(data_dir, gesture_labels, data_type)
+		self._data_dir = data_dir
+		self._data_type = data_type
+		# Initialize data locations for lazy loading
+		self.data, self.max_seq_len = self.populate_encoding_data(gesture_labels)
 		self.len = len(self.data)
 		logging.info('Initialized a ResnetEncodingDataset of size {0}.'.format(self.len))
 		super(ResnetEncodingDataset, self).__init__()
 
 	def __getitem__(self, idx):
 		# TODO(kenny): Figure out how to sample with balanced labels.
-		return self.data[idx]
+		item = self.data[idx]
+
+		# Tack on the (D, T) tensor representing the spatiotemporal frames for a video.
+		video_dir = os.path.join(self._data_dir, item['video_dir'])
+		item['frames'] = self.read_frame_tensors_from_dir(video_dir)
+		return item
 
 	def __len__(self):
 		return self.len
 
-	def populate_encoding_data(self, data_dir, gesture_labels, data_type):
+	def populate_encoding_data(self, gesture_labels):
 		"""Returns a list of dicts with keys:
-			'frames': 4D tensor (T, H, W, C) representing the spatiotemporal frames for a video
 			'label': y (ground truth)
 			'seq_len': number of frames in the video
+			'video_dir': data-dir-relative directory where videos are stored
 		"""
 		logging.info('Populating frame tensors for {0} specified labels in data dir {1}: {2}'.format(
-			len(gesture_labels), data_dir, gesture_labels))
+			len(gesture_labels), self._data_dir, gesture_labels))
 
 		if self._max_example_per_label:
 			logging.info('Max example per label: {}'.format(self._max_example_per_label))
@@ -49,20 +56,20 @@ class ResnetEncodingDataset(Dataset):
 		max_seq_len = -1
 
 		for label in gesture_labels:
-			label_dir = os.path.join(data_dir, str(label))
-			video_dirs = self.get_video_dirs(label_dir, data_type)
+			label_dir = os.path.join(self._data_dir, str(label))
+			video_dirs = self.get_video_dirs(label_dir)
 
 			# cap the number of images per label
 			if self._max_example_per_label:
 				video_dirs = video_dirs[:self._max_example_per_label]
 
-			logging.info('Reading frame tensors for label {0} ({1} video)'.format(label, len(video_dirs)))
+			logging.info('Assigning frame tensor locations for label: {0} ({1} videos)'.format(
+				label, len(video_dirs)))
 			for video_dir in video_dirs:
-				frames = self.read_frame_tensors_from_dir(os.path.join(data_dir, video_dir), data_type)
-				seq_len = frames.size(1)
+				# Keep track of the global max seq len for batch RNN unrolling purposes.
+				seq_len = len(glob.glob(os.path.join(video_dir, '*.png')))
 				max_seq_len = max(max_seq_len, seq_len)
 				data.append({
-					'frames': frames,
 					'label': self._labels_to_indices_dict[label],
 					'video_dir': video_dir,
 					'seq_len': seq_len
@@ -70,17 +77,17 @@ class ResnetEncodingDataset(Dataset):
 
 		return data, max_seq_len
 
-	def read_frame_tensors_from_dir(self, directory, data_type):
-		location = os.path.join(directory, '{}-encoding.pkl'.format(data_type))
+	def read_frame_tensors_from_dir(self, directory):
+		location = os.path.join(directory, '{}-encoding.pkl'.format(self._data_type))
 		return torch.load(location)
 
-	def get_video_dirs(self, label_dir, data_type):
+	def get_video_dirs(self, label_dir):
 
 		# return a list of paths for the images
 		prefix = None
-		if data_type.endswith('RGB'):
+		if self._data_type.endswith('RGB'):
 			prefix = 'M_'
-		elif data_type.endswith('RGBD'):
+		elif self._data_type.endswith('RGBD'):
 			prefix = 'K_'
 		else:
 			raise ValueError('Data type for pickling is invalid')
