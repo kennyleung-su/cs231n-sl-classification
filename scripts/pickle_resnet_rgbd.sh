be3c68e94ec7b35a94c5491736ffe358bc8380bf
seahyinghang8@gmail.com <seahy@cs231-vm.c.alpine-ship-200915.internal>
2018-06-07 05:23:25 +0000

    updated pickle rgbd script

--- scripts/pickle_resnet_rgbd.sh
+++ scripts/pickle_resnet_rgbd.sh
@@ -3,6 +3,7 @@
 
 set -e
 experiment='RN18_RGBD_final'
+checkpoint='RN18_RGBD_checkpoint.pkl'
 
 set -x
-python main.py --mode pickle --experiment ${experiment} --use_cuda --num_workers 32 --load --checkpoint_to_load # TODO: add checkpoint.
+python main.py --mode pickle --experiment ${experiment} --use_cuda --num_workers 32 --load --checkpoint_to_load ${checkpoint}
